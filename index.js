fb1522a023c75580318debeb88a3f96e893e95ec
Sheikh Tech Riz <rizwan.ndu@gmail.com>
2024-10-13 02:59:54 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// const fs = require('fs');
+
+
+// const app = require('./app')
+// console.log(app);
+// // console.log("sub");
+// // let x = 10;
+// // let y = 20;
+// // let z = 10;
+// // console.warn(x+y+z);
+// // console.error(x+y+z);
+
+
+// const arr = [1,3,2,7,8,4,2];
+
+// let result = arr.filter((item)=>{
+//     return item === 2
+// });
+// console.log(result);
+
+// file system function
+// fs.writeFileSync("hellotext", "Sheikh Rizwan Muntaha");
+// console.log(__dirname);
+
+
+// http function:
+
+// const http = require('http');
+
+// http.createServer((req , resp)=>{
+
+//     resp.write(" <h3> This is the Sheikh Rizwan Laptop </h3>");
+//     resp.end();
+
+// }).listen(4500);
+
+// Package json file create in terminal (npm init ) 
+const colors = require('colors');
+console.log("sheikh".red);
+
+const chalk = require('chalk');
+console.log(chalk.blue('muntaha'));
